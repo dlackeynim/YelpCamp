8d7dd84f197a809239355b7210e59ecd0be7ad25
Daniel Lackey <dcl24102@gmail.com>
2023-01-07 15:23:37 -0600

    Add route for campgrounds

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const campground = require('./models/campground');
-constCampground = require('./models/campground');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -25,12 +24,15 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new campground({ title: 'My Backyard', description: 'cheap camping!' });
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+   const campgrounds = await Campground.find({})
+   res.render('campgrounds/index', {campgrounds})
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
