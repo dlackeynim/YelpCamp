b101eee6c6bac2f738d731ccba7b8f1c5757d0c5
Daniel Lackey <dcl24102@gmail.com>
2023-01-10 11:50:18 -0600

    Seed DB with images and add pseudo description

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for( let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10
         const camp = new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/11433913',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, vel laboriosam. Animi praesentium voluptas odio incidunt eaque est quaerat sed reprehenderit, rem ex tenetur repudiandae officia eius totam ab magni.',
+            price
         })
         await camp.save();
     }
