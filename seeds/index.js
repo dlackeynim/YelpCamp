0553ef3057bfe6ac53b04f02676c79fcc4eb58a8
Daniel Lackey <dcl24102@gmail.com>
2023-01-24 15:11:33 -0600

    Implement author of campground, author authorization to edite/delete

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30) + 10
         const camp = new Campground({
+            author: '63d009ee8ef3017e5f274724',
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/11433913',
