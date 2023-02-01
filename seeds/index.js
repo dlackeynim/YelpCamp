c0ce1ad581f79e931b84f5c79bb4b9733239bd6b
Daniel Lackey <dcl24102@gmail.com>
2023-02-01 12:42:19 -0600

    Center mage on campground location

--- seeds/index.js
+++ seeds/index.js
@@ -23,11 +23,16 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30) + 10
         const camp = new Campground({
+            //YOUR USER ID
             author: '63d009ee8ef3017e5f274724',
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, vel laboriosam. Animi praesentium voluptas odio incidunt eaque est quaerat sed reprehenderit, rem ex tenetur repudiandae officia eius totam ab magni.',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                   url: 'https://res.cloudinary.com/drak25qkc/image/upload/v1675104112/YelpCamp/jrqxzbxvivglpsu2vjwp.jpg',
